82c11947be2a16ba4f638fe2bc05f5548b39876e
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-03 15:37:01 -0500

    agregando firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  firebase: { "projectId": "test-learning-28270", "appId": "1:620091757425:web:dc625aa732bd44b719dcd0", "storageBucket": "test-learning-28270.appspot.com", "apiKey": "AIzaSyDOTxou8i4o2fdroK4v6K7qsEV5p2nvorQ", "authDomain": "test-learning-28270.firebaseapp.com", "messagingSenderId": "620091757425" }
+};
