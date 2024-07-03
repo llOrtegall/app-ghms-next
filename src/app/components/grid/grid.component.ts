052b58447173a6ebb8ed4c8cdb8df25217de088c
llOrtegall <IvanOrtega_97@hotmail.com>
2024-07-03 15:41:29 -0500

    components creados

---
+++ src/app/components/grid/grid.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      grid works!
+    </p>
+  `,
+  styles: ``
+})
+export class GridComponent {
+
+}
