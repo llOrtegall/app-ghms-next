6278cdb1bd97fae7ac49f4c232b1f96d1a885f17
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-11 14:26:27 -0500

    first commit

--- src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  firebase: { "projectId": "test-learning-28270", "appId": "1:620091757425:web:dc625aa732bd44b719dcd0", "storageBucket": "test-learning-28270.appspot.com", "apiKey": "AIzaSyDOTxou8i4o2fdroK4v6K7qsEV5p2nvorQ", "authDomain": "test-learning-28270.firebaseapp.com", "messagingSenderId": "620091757425" }
-};
